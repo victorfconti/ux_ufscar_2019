6dacc7ed8d0c31ff7e11619d7437ed447145521c
= <victorfconti@gmail.com>
2020-04-19 16:14:56 -0300

    Criação da estrutura básica

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,12 @@
+
+D
+src/serviceWorker.js,9/2/920ab6ab4a57ee20a36df4e3e2e73b70f9969d14
+A
+public/index.html,f/4/f43ecdf8d666ee908aacce037b4cd72159e4741b
+<
+src/index.js,f/9/f900edc44cb929c2267a16100a32851f6d5b477d
+C
+src/SearchAppBar.js,d/0/d0ca527bab021560c9b680e713049a1a54831701
+:
+
+src/App.js,1/e/1e43fa63aecd2127be034b3fb9d4caa172275714
--- src/SearchAppBar.js
+++ src/SearchAppBar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import clsx from 'clsx';
 import {fade, makeStyles, useTheme} from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -126,10 +125,9 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function SearchAppBar() {
+export default function SearchAppBar({open, setOpen}) {
     const classes = useStyles();
     const theme = useTheme();
-    const [open, setOpen] = React.useState(window.innerWidth > 760);
 
     const handleDrawerOpen = () => {
         setOpen(true);
@@ -140,8 +138,7 @@ export default function SearchAppBar() {
     };
 
     return (
-        <div className={classes.root}>
-            <CssBaseline/>
+        <div>
             <AppBar
                 position="fixed"
                 className={clsx(classes.appBar, {
@@ -209,36 +206,6 @@ export default function SearchAppBar() {
                     ))}
                 </List>
             </Drawer>
-            <main
-                className={clsx(classes.content, {
-                    [classes.contentShift]: open,
-                })}
-            >
-                <div className={classes.drawerHeader}/>
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                    donec massa sapien faucibus et molestie ac.
-                </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-                </Typography>
-            </main>
         </div>
     );
 }
