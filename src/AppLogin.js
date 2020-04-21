bd428db9f813571df9d0fcea5cbdf31fd6ed95da
= <victorfconti@gmail.com>
2020-04-21 16:29:55 -0300

    Busca realizada

--- src/AppLogin.js
+++ src/AppLogin.js
@@ -2,9 +2,9 @@ import React from 'react';
 import SearchAppBar from "./SearchAppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import clsx from "clsx";
-import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/core/styles";
-import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const drawerWidth = 240;
 
@@ -55,34 +55,54 @@ function AppLogin() {
             <CssBaseline/>
             <SearchAppBar open={open} setOpen={setOpen}>
             </SearchAppBar>
-            <main
-                className={clsx(classes.content, {
-                    [classes.contentShift]: open,
-                })}
-            >
-                <div className={classes.drawerHeader}/>
-                <Breadcrumbs aria-label="breadcrumb">
-                    <Typography color="textPrimary">Acessibilidade</Typography>
-                </Breadcrumbs>
-                <div style={{align: 'center', alignSelf: 'center', alignContent: "center", textAlign: "center", alignItems: "center"}}>
-                    <h1>Acessibilidade</h1>
-                </div>
-                <Typography paragraph>
-                    Nosso projeto tem como finalidade prover uma plataforma onde os interessados no desenvolvimento de
-                    interfaces para o usuário possam estudar e se atualizar, de uma maneira prática e divertira, que
-                    une a teoria a prática.
-                </Typography>
-                <Typography paragraph>
-                    Para isso possuimos nossa seção de exemplo, onde iremos mostrar exemplos de criação de partes de
-                    um sistema. Exibindo seu resultado final, e o código de cada componente, além de explicações
-                    sobre as diretivas.
-                </Typography>
-                <Typography paragraph>
-                    Por fim temos uma sessão inteira voltada a dicas de acessibilidade que você pode usar no seu
-                    sistema, aumentando assim a experiencia do usuário e contemplando milhares de novos usuários.
-                </Typography>
-            </main>
+                 <main
+                     className={clsx(classes.content, {
+                         [classes.contentShift]: open,
+                     })}
+                 >
+                     <div className={classes.drawerHeader}/>
+                     <div style={{align: 'center', alignSelf: 'center', alignContent: "center", textAlign: "center", alignItems: "center"}}>
+                         <h2>Login</h2>
+                         <TextField id="standard-basic" label="Usuário" />
+                         <p/>
+                         <TextField label="Senha"  type="password"/>
+                         <p/>
+                         <Button style={{width: "220px"}} variant="contained" color="primary">Login</Button>
+                     </div>
+                 </main>
         </div>
+        // <div className={classes.root}>
+        //     <CssBaseline/>
+        //     <SearchAppBar open={open} setOpen={setOpen}>
+        //     </SearchAppBar>
+        //     <main
+        //         className={clsx(classes.content, {
+        //             [classes.contentShift]: open,
+        //         })}
+        //     >
+        //         <div className={classes.drawerHeader}/>
+        //         <Breadcrumbs aria-label="breadcrumb">
+        //             <Typography color="textPrimary">Acessibilidade</Typography>
+        //         </Breadcrumbs>
+        //         <div style={{align: 'center', alignSelf: 'center', alignContent: "center", textAlign: "center", alignItems: "center"}}>
+        //             <h1>Acessibilidade</h1>
+        //         </div>
+        //         <Typography paragraph>
+        //             Nosso projeto tem como finalidade prover uma plataforma onde os interessados no desenvolvimento de
+        //             interfaces para o usuário possam estudar e se atualizar, de uma maneira prática e divertira, que
+        //             une a teoria a prática.
+        //         </Typography>
+        //         <Typography paragraph>
+        //             Para isso possuimos nossa seção de exemplo, onde iremos mostrar exemplos de criação de partes de
+        //             um sistema. Exibindo seu resultado final, e o código de cada componente, além de explicações
+        //             sobre as diretivas.
+        //         </Typography>
+        //         <Typography paragraph>
+        //             Por fim temos uma sessão inteira voltada a dicas de acessibilidade que você pode usar no seu
+        //             sistema, aumentando assim a experiencia do usuário e contemplando milhares de novos usuários.
+        //         </Typography>
+        //     </main>
+        // </div>
     );
 }
 
